20ab8c36d0984b0fee15129b71b5817f5045f070
Ben <ben@notionparallax.co.uk>
2020-03-30 19:58:55 +1100

    keep the ports consistent

--- js/helpers.js
+++ js/helpers.js
@@ -1,7 +1,7 @@
 function contextAwareURL(url) {
   let h = window.location.host.split(":")[0];
   if (h == "127.0.0.1" || h == "localhost" || h == "0.0.0.0") {
-    url = "http://localhost:8000";
+    url = "http://localhost:8080";
   } else {
     url = "https://todo-ljb4cakpxq-uc.a.run.app";
   }
