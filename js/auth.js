a5cd38aec1831651512b6e4dba40eafe78076c36
Ben <ben@notionparallax.co.uk>
2020-04-02 22:56:35 +1100

    add github
    This will do until I get the nod from twitter, closes #12

--- js/auth.js
+++ js/auth.js
@@ -23,10 +23,10 @@ document.addEventListener(
       signInSuccessUrl: "home",
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
         // firebase.auth.EmailAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
