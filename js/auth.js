fcc0fd5966b50af161ae1d1f21c49defb31e3a35
Ben <ben@notionparallax.co.uk>
2020-04-14 14:54:44 +1000

    lint

--- js/auth.js
+++ js/auth.js
@@ -2,21 +2,21 @@ console.log("in auth.js");
 
 document.addEventListener(
   "DOMContentLoaded",
-  function() {
+  function () {
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
     var uiConfig = {
       callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
           // User successfully signed in.
           // Return type determines whether we continue the redirect automatically
           // or whether we leave that to developer to handle.
           return true;
         },
-        uiShown: function() {
+        uiShown: function () {
           // The widget is rendered, Hide the loader.
           document.getElementById("loader").style.display = "none";
-        }
+        },
       },
       // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
       signInFlow: "popup",
@@ -34,7 +34,7 @@ document.addEventListener(
       // Terms of service url.
       tosUrl: "tos",
       // Privacy policy url.
-      privacyPolicyUrl: "privacy"
+      privacyPolicyUrl: "privacy",
     };
 
     // The start method will wait until the DOM is loaded.
