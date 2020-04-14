1844fcc3ae2ce13ef2d742ac9c937cfe2024e199
Ben <ben@notionparallax.co.uk>
2020-04-14 14:54:36 +1000

    add facebook sign in

--- js/auth.js
+++ js/auth.js
@@ -24,9 +24,10 @@ document.addEventListener(
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        "microsoft.com",
         // firebase.auth.EmailAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
