ebaa95a80c051f5112dd02598e0d503e96795bc1
Ben <ben@notionparallax.co.uk>
2020-04-16 15:58:00 +1000

    little bits of tidying up

--- js/header.js
+++ js/header.js
@@ -1,23 +1,23 @@
-console.log("in header.js");
+// console.log("in header.js");
 let hamburger = document.querySelector(".hamburger svg");
 if (hamburger) {
-  console.log("set click on burger");
-  hamburger.onclick = function() {
+  // console.log("set click on burger");
+  hamburger.onclick = function () {
     document.querySelector(".profile").classList.toggle("menu-active");
   };
 }
 
 let signOutButton = document.getElementById("sign-out");
 if (signOutButton) {
-  signOutButton.onclick = function() {
+  signOutButton.onclick = function () {
     firebase
       .auth()
       .signOut()
-      .then(function() {
+      .then(function () {
         // Sign-out successful.
         window.location = "home";
       })
-      .catch(function(error) {
+      .catch(function (error) {
         // An error happened.
         console.error("Sign out error:", error);
       });
