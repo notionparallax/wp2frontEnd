11498c139390c4705f85c606f87b9ae7ce1bb9cf
Ben <ben@notionparallax.co.uk>
2020-04-30 10:28:08 +1000

    add email signup

--- js/auth.js
+++ js/auth.js
@@ -5,6 +5,8 @@ document.addEventListener(
   function () {
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+    var microsoftcock = new firebase.auth.OAuthProvider("microsoft.com");
+
     var uiConfig = {
       callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
@@ -24,11 +26,11 @@ document.addEventListener(
       signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
         "microsoft.com",
-        // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
       // Terms of service url.
