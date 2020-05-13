c2525a4dd2458c35918c98fa16978340eb9b76a7
Ben <ben@notionparallax.co.uk>
2020-05-13 11:48:47 +1000

    multiple tier payments, initial

--- js/payment.js
+++ js/payment.js
@@ -1,42 +1,66 @@
 window.addEventListener("userReady", function () {
-  var stripe = Stripe("pk_live_q1dcaunx7RfFnw9n8A6K3yHa");
+  if (window.location.href.includes("test")) {
+    var stripe = Stripe("pk_test_oqvBwVn1rocov0b3aHDawOWL");
+  } else {
+    var stripe = Stripe("pk_live_q1dcaunx7RfFnw9n8A6K3yHa");
+  }
 
-  var checkoutButton = document.querySelector("[id^='checkout-button-']");
-  var checkoutID = checkoutButton.id.split("checkout-button-")[1];
+  document
+    .querySelectorAll("[id^='checkout-button-']")
+    .forEach((checkoutButton) => {
+      const checkoutID = checkoutButton.id.split("checkout-button-")[1];
 
-  checkoutButton.addEventListener("click", function () {
-    // When the customer clicks on the button, redirect them to Checkout.
-    stripe
-      .redirectToCheckout({
-        items: [{ plan: checkoutID, quantity: 1 }],
+      checkoutButton.addEventListener("click", function () {
+        // When the customer clicks on the button, redirect them to Checkout.
+        stripe
+          .redirectToCheckout({
+            items: [{ plan: checkoutID, quantity: 1 }],
 
-        // Do not rely on the redirect to the successUrl for fulfilling
-        // purchases, customers may not always reach the success_url after
-        // a successful payment.
-        // Instead use one of the strategies described in
-        // https://stripe.com/docs/payments/checkout/fulfillment
-        successUrl: "https://waldenpond.press/home",
-        cancelUrl: "https://waldenpond.press/canceled",
-        customerEmail: window.wp_user.email,
-        clientReferenceId: window.wp_user.uid,
-        // billingAddressCollection: 'required',
-        shippingAddressCollection: {
-          // prettier-ignore
-          allowedCountries: ["AU",'CA','US',"AQ","AT","BE","BG","CY","CZ","DE",
-                             "DK","EE","ES","FI","FR","GB","GR","HU","IE","IT",
-                             "LT","LU","LV","MT","NL","PL","PT","RO","SE","SI",
-                             "SK",]
-        },
-      })
-      .then(function (result) {
-        if (result.error) {
-          // If `redirectToCheckout` fails due to a browser or network
-          // error, display the localized error message to your customer.
-          var displayError = document.getElementById("error-message");
-          displayError.textContent = result.error.message;
-        }
+            // Do not rely on the redirect to the successUrl for fulfilling
+            // purchases, customers may not always reach the success_url after
+            // a successful payment.
+            // Instead use one of the strategies described in
+            // https://stripe.com/docs/payments/checkout/fulfillment
+            successUrl: "https://waldenpond.press/home",
+            cancelUrl: "https://waldenpond.press/canceled",
+            customerEmail: window.wp_user.email,
+            clientReferenceId: window.wp_user.uid,
+            // billingAddressCollection: 'required',
+            shippingAddressCollection: {
+              // prettier-ignore
+              allowedCountries: ["AU",'CA','US',"AQ","AT","BE","BG","CY","CZ","DE",
+                               "DK","EE","ES","FI","FR","GB","GR","HU","IE","IT",
+                               "LT","LU","LV","MT","NL","PL","PT","RO","SE","SI",
+                               "SK",]
+            },
+          })
+          .then(function (result) {
+            if (result.error) {
+              // If `redirectToCheckout` fails due to a browser or network
+              // error, display the localized error message to your customer.
+              var displayError = document.getElementById("error-message");
+              displayError.textContent = result.error.message;
+            }
+          });
       });
-  });
+      checkoutButton.classList.remove("disabled");
+    });
 
-  checkoutButton.classList.remove("disabled");
+  const managePaymentButton = document.getElementById("manage-payment-button");
+  // .setAttribute("action", contextAwareURL() + "/create_billing_portal");
+  managePaymentButton.addEventListener("click", () => {
+    let body = JSON.stringify(window.wp_user);
+    fetch(contextAwareURL() + "/create_billing_portal", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: { "Content-Type": "application/json" },
+      body: body, // body data type must match "Content-Type" header
+      //docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        window.location.replace(data.session.url);
+      })
+      .catch((e) => console.error("fetch failed", e));
+  });
 });
--- payment.md
+++ payment.md
@@ -11,15 +11,65 @@ This is a subscription for 1 issue of Walden Pond every month.
 
 <!-- Create a button that your customers click to complete their purchase. Customize the styling to suit your branding. -->
 
+# printed
+
 <button
   style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
-  id="checkout-button-plan_H6dC7DyEWCjLqi"
+  id="checkout-button-plan_HGK3aYMCcwkWcM"
   role="link"
-  class="disabled">Subscribe with Stripe</button>
+  class="disabled">
+<span class="product-name">1 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-plan_HGK2EUGJYdT4Ug"
+  role="link"
+  class="disabled">
+<span class="product-name">2 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-plan_HGJyibrCINh1Uf"
+  role="link"
+  class="disabled">
+<span class="product-name">4 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
+
+# PDF
+
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-plan_HGK3aYMCcwkWcM"
+  role="link"
+  class="disabled">
+<span class="product-name">1 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-plan_HGK2EUGJYdT4Ug"
+  role="link"
+  class="disabled">
+<span class="product-name">2 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
+<button
+  style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+  id="checkout-button-plan_HGK2lal9ACxPNz"
+  role="link"
+  class="disabled">
+<span class="product-name">4 hour</span>
+<span class="sub-with-stripe">Subscribe with Stripe</span>
+</button>
 
 <div id="error-message"></div>
 
 Currently there isn't a button to automatically cancel your subscription, which is a total dick move, but this link will send an email to me, and I'll do it for you: [cancel my subscription](mailto:ben@notionparallax.co.uk?cc=user@email.com&subject=Cancel my Walden Pond subscription please&body=No hard feelings)
+
+<button id="manage-payment-button">Manage billing</button>
 
 {% include base-scripts.md %}
 
