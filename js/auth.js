142fc45403adf6e1ca256bfd91ae5fba94881967
Ben <ben@notionparallax.co.uk>
2020-05-13 11:48:57 +1000

    allow twitter sign in

--- js/auth.js
+++ js/auth.js
@@ -30,7 +30,7 @@ document.addEventListener(
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         "microsoft.com",
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
       // Terms of service url.
