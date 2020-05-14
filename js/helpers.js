b9c640cb5b81e6a66e627a9c8bf09144f7d3691a
Ben <ben@notionparallax.co.uk>
2020-05-14 16:55:11 +1000

    update to new API url

--- js/helpers.js
+++ js/helpers.js
@@ -3,7 +3,8 @@ function contextAwareURL(url) {
   if (h == "127.0.0.1" || h == "localhost" || h == "0.0.0.0") {
     url = "http://localhost:8080";
   } else {
-    url = "https://todo-ljb4cakpxq-uc.a.run.app";
+    // url = "https://todo-ljb4cakpxq-uc.a.run.app";
+    url = "https://api.waldenpond.press/";
   }
   return url;
 }
