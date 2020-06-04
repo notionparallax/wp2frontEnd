1c2c8554571a5cd021ec51bb2a15cdd351062ee4
Ben <ben@notionparallax.co.uk>
2020-06-04 14:17:59 +1000

    remove facebook login until they start being adults

--- js/auth.js
+++ js/auth.js
@@ -27,9 +27,9 @@ document.addEventListener(
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        "microsoft.com",
+        // "microsoft.com",
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
