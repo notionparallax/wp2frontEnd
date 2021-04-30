ab1e9706de54358dd539d0d09c2b9d04a6d1af48
Ben <ben@notionparallax.co.uk>
2021-04-30 13:17:53 +1000

    add basic events to link drawer

--- js/linkExpander.js
+++ js/linkExpander.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   let lx = document.querySelector(".link-expander");
+
   lx.querySelector("button").addEventListener("click", (e) => {
     e.preventDefault();
+    const newState = Array.from(lx.classList).includes("out") ? "in" : "out";
+    alert(newState);
+    gtag("event", "shortlink_drawer_" + newState);
     lx.classList.toggle("out");
   });
+
   lx.querySelector("input").addEventListener("input", (e) => {
     let goLink = lx.querySelector("a");
     if (e.target.value.length >= 4) {
@@ -12,5 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       goLink.classList.remove("live");
     }
+  });
+
+  lx.querySelector("a").addEventListener("click", (e) => {
+    code = lx.querySelector("input").value;
+    gtag("event", "shortlink_expanded", { link: code });
   });
 });
